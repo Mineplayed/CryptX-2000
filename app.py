89d1d1f74c8172004a34cdb7cd46055ec02bae0a
Mineplayed <83216970+Mineplayed@users.noreply.github.com>
2025-01-11 19:45:39 +0100

    Update app.py
    I polished the ceasar and rot13 cyphers. I started to explore a way to encrypt using the vigenere cypher and must admit it's real fun but lot of mind struggles. But I got the ascii code for the key and the message just need to merge them and transform them into characters.

--- app.py
+++ app.py
@@ -1,31 +1,74 @@
-list = input("Indiquez votre message à crypter : ")
+clear_message = input("Indiquez votre message à crypter : ")
+result = ""
 
-# reprendre avec le tableau asscii utf-8
-
-def ROT13(list):
-    rot13 = str.maketrans(
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-        "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm")
-    print(list.translate(rot13))
-    
-ROT13(list)
-
-def encrypt(list):
-    offset = int(input("Indiquez le décalage : "))
-    result = ""
-
-    # traverse text
-    for i in range(len(list)):
-        char = list[i]
-
-        # Encrypt uppercase characters
-        if (char.isupper()):
-            result += chr((ord(char) + offset - 65) % 26 + 65)
-
-        # Encrypt lowercase characters
+# Function to encrypte or decrypt message in ROT13
+def ROT13(clear):    
+    # Loop for each letters
+    for i in range(len(clear)):
+        letter = clear[i]  
+        # Encrypt or decrypt lower letters
+        if (letter.islower()):
+            result += chr((ord(letter) - 84) % 26 + 97)
+        # Encrypt or decrypt upper letters
         else:
-            result += chr((ord(char) + offset - 97) % 26 + 97)
-            
+            result += chr((ord(letter) - 52) % 26 + 65)
     print(result)
+# ROT13(clear_message)
 
-encrypt(list)
+# Function to encrypt or decrypt message in Caesar Cipher
+def Caesar(clear):
+    # Ask for the offset of the encrypting
+    offset = int(input("Indiquez le décalage : "))
+    
+    # Loop for each letters 
+    for i in range(len(clear)):
+        letter = clear[i]
+        # Encrypt or decrypt lower letters
+        if (letter.islower()):
+            result += chr((ord(letter) + offset - 97) % 26 + 97)
+        # Encrypt or decrypt upper letters
+        else:
+            result += chr((ord(letter) + offset - 65) % 26 + 65)
+    print(result)
+# Caesar(clear_message)
+
+# Function to encrypt or decrypt message in Vigenere Cipher
+def Vigenere(clear):
+    sliced_clear = []
+    sliced_key = []
+    repeated_key = []
+    key = str(input("Entrer votre clé de cryptage : "))
+    
+    # Separate all letters of the key
+    count = 0
+    for i in key:
+        sliced_key.append(key[count])
+        count += 1
+        
+    count = 0
+    for i in clear:
+        sliced_clear.append(clear[count])
+        count += 1
+    print(sliced_key, sliced_clear)
+    
+    count = 0
+    for i in sliced_clear:
+        if count < len(sliced_key):
+            repeated_key.append(sliced_key[count])  
+        else:
+            count = 0
+            repeated_key.append(sliced_key[count])
+        count += 1
+        
+    repeated_number_clear = []
+    count = 0
+    for i in repeated_key:
+        char = sliced_clear[count]
+        count += 1
+        if (char.islower()):
+            repeated_number_clear.append(str((ord(char) - 97) % 26 + 97))
+        else:
+            repeated_number_clear.append(str((ord(char) - 65) % 26 + 65))
+    print(repeated_number_clear)
+
+Vigenere(clear_message)
